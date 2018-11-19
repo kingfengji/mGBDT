7259078ef6a41a77cd72671dc081c60adeb2ef6e
kingfengji <kingfengji@gmail.com>
2018-11-19 15:27:38 +0800

    add scripts for downloading dataset

---
+++ datasets/uci_adult/get_data.sh
@@ -0,0 +1,2 @@
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test
---
+++ datasets/uci_yeast/get_data.sh
@@ -0,0 +1,1 @@
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.data
